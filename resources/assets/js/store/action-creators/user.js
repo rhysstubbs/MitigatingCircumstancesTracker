23b4806f374d15ed4d52b298958dd556cc45e65b
Rhys Stubbs <rhys@soundintheory.co.uk>
2019-01-05 23:28:47 +0000

    Remove comments and prod asset build

--- resources/assets/js/store/action-creators/user.js
+++ resources/assets/js/store/action-creators/user.js
@@ -1,26 +1,18 @@
-//import {OK} from 'http-status-codes';
-//import axios from 'axios';
+import {OK} from 'http-status-codes';
+import axios from 'axios';
 import {removeNotification} from 'MCT/store/actions/index';
-
-//import {API_URL} from "MCT/utils/api";
+import {API_URL} from "MCT/utils/api";
 
 const clearNotification = (payload) => {
     return function (dispatch) {
-
-        return new Promise(function (resolve) {
-            dispatch(removeNotification(payload.id));
-
-            resolve();
-        });
-
-        /**return axios.delete(`${API_URL}/user/${payload.username}/notifications/${payload.id}`)
-         .then(response => {
+        return axios.delete(`${API_URL}/user/${payload.username}/notifications/${payload.id}`)
+            .then(response => {
                 switch (response.status) {
                     case OK:
                         dispatch(removeNotification(payload.id));
                         return response;
                 }
-            })*/
+            })
     }
 };
 
